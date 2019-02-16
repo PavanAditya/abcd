56bc2402fc5535428d37b401f903957ff17a5dfa
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2019-02-16 16:15:56 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+function drag(e) {
+    element = e.target.id
+}
+function allowDrop(e) {
+    e.preventDefault();
+}
+function drop(e) {
+    e.target.appendChild(document.getElementById(element))
+}
+
+
+// on drag start - to card - every card call drag()
+// on drag over - to coloumn - calls allowDrop()
+// on drop - to coloumn - calls drop()
